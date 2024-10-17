998624a0515d8b81667a6f82db2522bbc6e217c5
Andrea Carulli <38762811+Blastd@users.noreply.github.com>
2024-10-17 22:39:52 +0200

    Migration script improvements (#11)
    * Table migration adjustments
    
    - Added membership_info
    - Removed billing_info
    - Removed tags
    
    * Table migration adjustments

--- db/migrations/000001_model.down.sql
+++ db/migrations/000001_model.down.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS events;
 DROP TABLE IF EXISTS "groups";
-DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS fursuits_events;
 DROP TABLE IF EXISTS authentications;
@@ -10,5 +9,6 @@ DROP TABLE IF EXISTS rooms;
 DROP TABLE IF EXISTS user_group;
 DROP TABLE IF EXISTS fursuits;
 DROP TABLE IF EXISTS room_guests;
-DROP TABLE IF EXISTS billing_info;
+DROP TABLE IF EXISTS membership_info;
 DROP TABLE IF EXISTS membership_cards;
+DROP TABLE IF EXISTS group_permissions;
