a6b8a22cad9e7ab10cbabca4fa31added3bb901e
Stark <starkthedragon@outlook.com>
2024-11-06 21:40:59 +0100

    Revert "Revert "Implement Virtual Threads""
    This reverts commit 86dd925c6ff9cd6526b428c2cee90e313aaaeff6.

--- application/src/main/java/net/furizon/backend/infrastructure/security/configuration/SecurityThreadConfiguration.java
+++ application/src/main/java/net/furizon/backend/infrastructure/security/configuration/SecurityThreadConfiguration.java
@@ -19,7 +19,7 @@ public class SecurityThreadConfiguration {
     @Bean(SESSION_THREAD_POOL_TASK_EXECUTOR)
     public Executor sessionThreadPoolTaskExecutor() {
         final var corePoolUpdateSize = securityConfig.getSession().getCorePoolUpdateSize();
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        final var executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(corePoolUpdateSize);
         executor.setMaxPoolSize(corePoolUpdateSize);
         executor.setThreadNamePrefix("session-thread-");
