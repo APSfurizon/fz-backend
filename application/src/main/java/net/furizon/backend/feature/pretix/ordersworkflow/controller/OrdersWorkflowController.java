1b898fe5f1a88a21f17e6e116d86e2b9132e6fcf
Luca Sorace "Stranck <strdjn@gmail.com>
2024-11-18 19:41:26 +0100

    Generating shop link done
    Needs to be tested

--- application/src/main/java/net/furizon/backend/feature/pretix/ordersworkflow/controller/OrdersWorkflowController.java
+++ application/src/main/java/net/furizon/backend/feature/pretix/ordersworkflow/controller/OrdersWorkflowController.java
@@ -1,13 +1,14 @@
 package net.furizon.backend.feature.pretix.ordersworkflow.controller;
 
 import lombok.RequiredArgsConstructor;
-/*import net.furizon.backend.feature.authentication.dto.LogoutUserResponse;
 import net.furizon.backend.feature.pretix.ordersworkflow.dto.LinkResponse;
+import net.furizon.backend.feature.pretix.ordersworkflow.usecase.GeneratePretixShopLink;
+import net.furizon.backend.infrastructure.pretix.service.PretixInformation;
 import net.furizon.backend.infrastructure.security.FurizonUser;
 import net.furizon.backend.infrastructure.usecase.UseCaseExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;*/
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,18 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/orders-workflow")
 @RequiredArgsConstructor
 public class OrdersWorkflowController {
-    //private final UseCaseExecutor executor;
+    private final UseCaseExecutor executor;
+    private final PretixInformation pretixService;
 
-    /*@GetMapping("/generate-pretix-shop-link")
+    @GetMapping("/generate-pretix-shop-link")
     public LinkResponse generatePretixShopLink(
             @AuthenticationPrincipal @NotNull final FurizonUser user
     ) {
         return executor.execute(
-
+                GeneratePretixShopLink.class,
+                new GeneratePretixShopLink.Input(user, pretixService)
         );
     }
 
-    @GetMapping("/get-order-edit-link")
+    /*@GetMapping("/get-order-edit-link")
     public LogoutUserResponse logoutUser(
             @AuthenticationPrincipal @NotNull final FurizonUser user
     ) {
