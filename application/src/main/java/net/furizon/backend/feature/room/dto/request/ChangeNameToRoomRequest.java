dfdd9df735d6b5c2ded03bda894fb1cdd8cbba43
Luca Sorace "Stranck <strdjn@gmail.com>
2024-12-11 13:59:30 +0100

    Added room name prefix

--- application/src/main/java/net/furizon/backend/feature/room/dto/request/ChangeNameToRoomRequest.java
+++ application/src/main/java/net/furizon/backend/feature/room/dto/request/ChangeNameToRoomRequest.java
@@ -3,6 +3,7 @@ package net.furizon.backend.feature.room.dto.request;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -14,5 +15,6 @@ public class ChangeNameToRoomRequest {
     @NotNull
     @NotEmpty
     @Size(min = 1, max = 254)
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\p{M}_\\-/!\"'()\\[\\].,&\\\\? ]{4,63}$")
     private final String name;
 }
--- application/src/main/java/net/furizon/backend/feature/room/dto/request/CreateRoomRequest.java
+++ application/src/main/java/net/furizon/backend/feature/room/dto/request/CreateRoomRequest.java
@@ -2,6 +2,7 @@ package net.furizon.backend.feature.room.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -10,5 +11,6 @@ public class CreateRoomRequest {
     @NotNull
     @NotEmpty
     @Size(min = 1, max = 254)
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\p{M}_\\-/!\"'()\\[\\].,&\\\\? ]{4,63}$")
     private final String name;
 }
