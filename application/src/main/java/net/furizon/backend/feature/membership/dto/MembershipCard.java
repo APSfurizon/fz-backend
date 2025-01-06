2b410c5e9d28fe740bb4f6c83ef6b3af6c125329
Luca Sorace "Stranck <strdjn@gmail.com>
2025-01-06 23:28:16 +0100

    Added custom cardNo format to membership cards

--- application/src/main/java/net/furizon/backend/feature/membership/dto/MembershipCard.java
+++ application/src/main/java/net/furizon/backend/feature/membership/dto/MembershipCard.java
@@ -1,6 +1,7 @@
 package net.furizon.backend.feature.membership.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,11 @@ public class MembershipCard {
     @Min(1L)
     private final int idInYear;
 
+    @Nullable
+    @Builder.Default
+    //This respects Furizon's card numbering. You may want to change the constructor with your impl
+    private final String cardNo = null;
+
     @Min(1970L)
     private final short issueYear;
 
@@ -28,5 +34,18 @@ public class MembershipCard {
     //If the membership card has been already registered in the external italian portal
     public boolean isRegistered() {
         return isRegistered;
+    }
+
+    public MembershipCard(long cardId, int idInYear, short issueYear,
+                          long userOwnerId, @Nullable Long createdForOrderId, boolean isRegistered) {
+        this.cardId = cardId;
+        this.idInYear = idInYear;
+        this.issueYear = issueYear;
+        this.userOwnerId = userOwnerId;
+        this.createdForOrderId = createdForOrderId;
+        this.isRegistered = isRegistered;
+
+        int year = (int) issueYear % 100;
+        cardNo = String.format("%02d%02d%03d", year, year + 1, idInYear);
     }
 }
