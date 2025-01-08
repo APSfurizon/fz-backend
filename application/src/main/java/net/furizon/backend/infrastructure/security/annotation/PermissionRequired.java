b5e6fcc5ef830bd550264fe1b7861d368f7f8ad5
Luca Sorace "Stranck <strdjn@gmail.com>
2025-01-08 13:51:13 +0100

    Fixed checkstyle

--- application/src/main/java/net/furizon/backend/infrastructure/security/annotation/PermissionRequired.java
+++ application/src/main/java/net/furizon/backend/infrastructure/security/annotation/PermissionRequired.java
@@ -3,7 +3,11 @@ package net.furizon.backend.infrastructure.security.annotation;
 import net.furizon.backend.infrastructure.security.permissions.Permission;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
