7c9521bf70b2a61bf759750f6d46d66660aa6bfa
Luca Sorace "Stranck <strdjn@gmail.com>
2025-01-10 19:22:58 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+set -e #crashes the script on errors
 
-docker build -t fz-backend .
+echo "pulling from git"
+git pull origin master
+echo "stopping service"
+service fz-backend stop
+echo "building docker"
+docker build --no-cache -t fz-backend .
+echo "Starting service"
+service fz-backend start
+service fz-backend status
