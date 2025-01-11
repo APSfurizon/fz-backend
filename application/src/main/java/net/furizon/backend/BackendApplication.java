a0e70142cba34ebd48522e6faf5c7ac019fa0992
Luca Sorace "Stranck <strdjn@gmail.com>
2025-01-11 16:16:55 +0100

    Made @Schedule actually run

--- application/src/main/java/net/furizon/backend/BackendApplication.java
+++ application/src/main/java/net/furizon/backend/BackendApplication.java
@@ -3,7 +3,9 @@ package net.furizon.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class BackendApplication {
