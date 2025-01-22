097f4ee7d38c380b02014ab6f31732118b3a372a
Luca Sorace "Stranck <strdjn@gmail.com>
2025-01-22 10:23:42 +0100

    Added getCurrentEvent endpoint

--- application/src/main/java/net/furizon/backend/feature/pretix/objects/event/controller/PretixEventController.java
+++ application/src/main/java/net/furizon/backend/feature/pretix/objects/event/controller/PretixEventController.java
@@ -6,6 +6,7 @@ import net.furizon.backend.feature.pretix.objects.event.Event;
 import net.furizon.backend.feature.pretix.objects.event.dto.GetEventsResponse;
 import net.furizon.backend.feature.pretix.objects.event.usecase.GetAllEventsUseCase;
 import net.furizon.backend.feature.pretix.objects.event.usecase.GetSingleEventUseCase;
+import net.furizon.backend.infrastructure.pretix.service.PretixInformation;
 import net.furizon.backend.infrastructure.usecase.UseCaseExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,11 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
 public class PretixEventController {
+    private final PretixInformation pretixInformation;
     private final UseCaseExecutor executor;
 
     @GetMapping("/")
     public GetEventsResponse getEvents() {
         return executor.execute(GetAllEventsUseCase.class, 0);
+    }
+
+    @GetMapping("/current")
+    public Event getCurrent() {
+        return pretixInformation.getCurrentEvent();
     }
 
     @GetMapping("/{eventId}")
