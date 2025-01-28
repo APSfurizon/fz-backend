7997a4a71d927e65b017e6c57161693b49f76fe9
Drew <carullic@outlook.it>
2025-01-28 22:18:52 +0100

    Added translation to countries response

--- application/src/main/java/net/furizon/backend/feature/pretix/objects/states/CountryData.java
+++ application/src/main/java/net/furizon/backend/feature/pretix/objects/states/CountryData.java
@@ -3,14 +3,20 @@ package net.furizon.backend.feature.pretix.objects.states;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
 public class CountryData extends PretixState {
     @NotNull String phonePrefix;
+    @Nullable Map<String, String> translatedDescription;
 
-    public CountryData(@NotNull String name, @NotNull String code, @NotNull String phonePrefix) {
+    public CountryData(@NotNull String name, @NotNull String code, @NotNull String phonePrefix,
+                       @Nullable Map<String, String> translatedDescription) {
         super(code, name);
         this.phonePrefix = phonePrefix;
+        this.translatedDescription = translatedDescription;
     }
 }
--- application/src/main/java/net/furizon/backend/feature/pretix/objects/states/dto/CountryDataRaw.java
+++ application/src/main/java/net/furizon/backend/feature/pretix/objects/states/dto/CountryDataRaw.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import net.furizon.backend.feature.pretix.objects.states.CountryData;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Locale;
+import java.util.Map;
+
 @Data
 public class CountryDataRaw {
     private final String name;
@@ -15,7 +18,11 @@ public class CountryDataRaw {
     @NotNull private final String phonePrefix;
 
     public CountryData toPhoneCountry() {
-        return new CountryData(name, code, phonePrefix);
+        var loc = Locale.of("", code);
+        Map<String, String> translatedDescription = Map.of(
+                Locale.ITALIAN.getLanguage(), loc.getDisplayCountry(Locale.ITALIAN),
+                Locale.ENGLISH.getLanguage(), loc.getDisplayCountry(Locale.ENGLISH));
+        return new CountryData(name, code, phonePrefix, translatedDescription);
     }
 
 }
