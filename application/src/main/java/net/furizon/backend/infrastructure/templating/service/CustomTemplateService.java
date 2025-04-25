a34210f234bc29d7630f3b876d92f4bf6e96ae44
Luca Sorace "Stranck <strdjn@gmail.com>
2025-04-25 17:46:19 +0200

    Cleaned code

--- application/src/main/java/net/furizon/backend/infrastructure/templating/service/CustomTemplateService.java
+++ application/src/main/java/net/furizon/backend/infrastructure/templating/service/CustomTemplateService.java
@@ -5,18 +5,13 @@ import gg.jte.ContentType;
 import gg.jte.TemplateOutput;
 import gg.jte.output.StringOutput;
 import gg.jte.resolve.DirectoryCodeResolver;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import net.furizon.backend.infrastructure.admin.AdminConfig;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -26,39 +21,21 @@ public class CustomTemplateService {
     @NotNull private final AdminConfig adminConfig;
     @NotNull private final TemplateEngine templateEngine;
 
-    public CustomTemplateService(@NotNull AdminConfig adminConfig) throws MalformedURLException {
-        final Path templatesPath = Paths.get(adminConfig.getCustomJteTemplatesLocation());
-        final URL templatesUrl = templatesPath.toFile().toURI().toURL();
-        log.info("Loading custom templates from {}", templatesUrl);
+    public CustomTemplateService(@NotNull AdminConfig adminConfig) {
         this.adminConfig = adminConfig;
+        final Path templatesPath = Paths.get(adminConfig.getCustomJteTemplatesLocation());
+        log.info("Loading custom templates from '{}' using jte jar '{}'",
+                templatesPath, this.adminConfig.getJteRuntimeJarLocation());
         this.templateEngine = TemplateEngine.create(
                 new DirectoryCodeResolver(templatesPath),
                 templatesPath,
                 ContentType.Html,
                 this.getClass().getClassLoader()
-                //new URLClassLoader(new URL[] {
-                //    templatesUrl,
-                //    URI.create("file:/app/templates/jte/jte-runtime-3.1.15.jar").toURL()
-                //})
         );
-        log.error("DIOCANEEEE " + System.getProperty("java.class.path"));
-        //this.templateEngine.setClassPath(List.of(templatesPath.toString(),
-        //    "jar:nested:C:/Users/Stran/Desktop/shit/programming/Furizon/fz-backend/
-        //    application/target/application-0.0.1-SNAPSHOT.jar/!BOOT-INF/lib/jte-runtime-3.1.15.jar!"));
-        //    "C:/Users/Stran/.m2/repository/gg/jte/jte-runtime/3.1.15/jte-runtime-3.1.15.jar"));
-        this.templateEngine.setClassPath(List.of(templatesPath.toString(),
-                "/app/templates/jte/jte-runtime-3.1.15.jar"));
-    }
-
-    @PostConstruct
-    public void init() {
-        try {
-            log.error("AAAAAAAAAAAAAAAAAAAA " + this.getClass().getClassLoader().getClass().getName());
-            log.error("AAAAAAAAAAAAAAAA "
-                    + Arrays.toString(((URLClassLoader) this.getClass().getClassLoader()).getURLs()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        this.templateEngine.setClassPath(List.of(
+                templatesPath.toString(),
+                this.adminConfig.getJteRuntimeJarLocation()
+        ));
     }
 
     public String renderTemplate(String templateName, Map<String, Object> params) {
