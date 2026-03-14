d58f6893d1266bf4b4e48c75a1adadc84d7bc7d2
Luca Sorace "Stranck <strdjn@gmail.com>
2026-03-14 18:03:05 +0100

    Update GalleryUpload.java

--- application/src/main/java/net/furizon/backend/feature/gallery/dto/GalleryUpload.java
+++ application/src/main/java/net/furizon/backend/feature/gallery/dto/GalleryUpload.java
@@ -18,18 +18,28 @@ import java.time.LocalDateTime;
 public class GalleryUpload {
     private final long id;
     @NotNull private final UserDisplayData photographer;
+
     @NotNull private final LocalDateTime uploadDate;
     @Nullable private final LocalDateTime shotDate;
+
     @NotNull private final UploadStatus status;
+
     @NotNull private final String fileName;
     private final long fileSize;
     private final int width;
     private final int height;
     @NotNull private final UploadType type;
+
     @NotNull private final MediaResponse media;
     @Nullable private final MediaResponse renderedMedia;
     @Nullable private final MediaResponse thumbnailMedia;
+
     private boolean isSelected;
+
     @NotNull private final UploadRepostPermissions repostPermissions;
+
     @NotNull private final Event event;
+
+    @Nullable private UploadImageMetadata photoMetadata;
+    @Nullable private UploadVideoMetadata videoMetadata;
 }
